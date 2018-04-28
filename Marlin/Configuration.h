36cf1f3270d99c7c6a94d71fae7eb4a21bb445b1
MirceaDan <ByReaL@users.noreply.github.com>
2018-04-28 15:39:55 -0700

    bondtech QR3 installed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -37,7 +37,7 @@
  */
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
-#define CONFIGURATION_H_VERSION 240418
+#define CONFIGURATION_H_VERSION 280418
 
 //===========================================================================
 //============================= Getting Started =============================
@@ -74,7 +74,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Mircea, 280418)" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
@@ -534,7 +534,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 841 }
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 841 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 502.5 }
 
 /**
  * Default Max Feed Rate (mm/s)
